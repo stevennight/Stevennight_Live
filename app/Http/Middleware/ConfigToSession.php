fe9e1e7cdfc2545130008094b7edf6c774cd125a
StevenNight <stevennight@qq.com>
2017-02-23 21:21:49 +0800

    Fixed some error annotation.

--- app/Http/Middleware/ConfigToSession.php
+++ app/Http/Middleware/ConfigToSession.php
@@ -12,6 +12,7 @@ class ConfigToSession{
             return redirect()->route('error')->with(['error' => 'Can not get any config from datebase.']);
         }
 
+        //如果用户登录了，刷新用户的信息。（确保封禁等行为的及时处理。）  refresh user's information when user has been login, for handle user immediatly like logout when users was baned.
         if($request->session()->has('member')){
             $db_user = Users::where('id','=',$request->session()->get('member.userid'))->first();
             Functions::refreshUserInfoIntoSession($db_user);
--- app/Http/Middleware/LoginStatusCheck.php
+++ app/Http/Middleware/LoginStatusCheck.php
@@ -7,14 +7,13 @@ use App\Custom\Functions;
 class LoginStatusCheck{
     public function handle($request, \Closure $next){
 
-        if($request->is('login')||$request->is('register')){
+        if($request->is('login')){
             if($request->session()->has('member')){
                 return redirect() -> route('index');
             }
             return $next($request);
         }
 
-        //如果用户登录了，刷新用户的信息。（确保封禁等行为的及时处理。）  refresh user's information when user has been login, for handle user immediatly like logout when users was baned.
         if(!$request->session()->has('member')){
             return redirect() -> route('index')->withErrors(['must_login' => trans('view.login.must_login')]);
         }
